afd6ec7d304256185dcfc5d6cc1a78b10cb92307
vaibjain11 <141463293+vaibjain11@users.noreply.github.com>
2024-08-06 21:25:33 -0500

    Update cron.js

--- backend/cron.js
+++ backend/cron.js
@@ -31,6 +31,3 @@ export default job;
 //? EXAMPLES && EXPLANATION:
 //* 14 * * * * - Every 14 minutes
 //* 0 0 * * 0 - At midnight on every Sunday
-//* 30 3 15 * * - At 3:30 AM, on the 15th of every month
-//* 0 0 1 1 * - At midnight, on January 1st
-//* 0 * * * * - Every hour
