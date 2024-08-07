4fa0ceca714d01e9b328fd91c24b02784afdc1fd
vaibjain11 <141463293+vaibjain11@users.noreply.github.com>
2024-08-06 21:24:39 -0500

    Update user.resolver.js

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -19,7 +19,6 @@ const userResolver = {
 				const salt = await bcrypt.genSalt(10);
 				const hashedPassword = await bcrypt.hash(password, salt);
 
-				// https://avatar-placeholder.iran.liara.run/
 				const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 				const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
